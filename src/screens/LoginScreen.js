71c5c3ce17dd0132434f8e11dd0e5751deea1058
gabims029 <gabimelo541@gmail.com>
2025-03-31 09:38:29 -0300

    Finalizado

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,19 +1,36 @@
 import React, { useState } from "react";
-import {View, Text, TextInput, TouchableOpacity, Alert, StyleSheet, Button, Image} from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  StyleSheet,
+  Button,
+  Image,
+} from "react-native";
 import api from "../axios/axios";
-import Logo from "../../assets/logosenai.png"
 
-export default function Login({ navigation }) {
+import Logo from "../../assets/logosenai.png";
+
+import { Ionicons } from "@expo/vector-icons";
+
+import {useNavigation} from "@react-navigation/native"
+
+export default function Login() {
   const [user, setUser] = useState({
     email: "",
-    password: "",
+    senha: "",
+    showPassword: true,
   });
+
+  const navigation = useNavigation()
 
   async function handleLogin() {
     await api.postLogin(user).then(
       (response) => {
         Alert.alert("OK", response.data.message);
-        navigation.navigate("Init")
+        navigation.navigate("Init");
       },
       (error) => {
         Alert.alert("Erro", error.response.data.error);
@@ -21,83 +38,69 @@ export default function Login({ navigation }) {
     );
   }
   return (
-    <View style={styles.container}>
-      {/* Header */}
-      <View style={styles.header}>
-      </View>
-
-      {/* Conteúdo principal */}
-      <View style={styles.content}>
-        <View style={styles.loginCard}>
-          <View style={styles.logoContainer}>
-          <View style={styles.logoContainer}>
-          <Image 
-          source={require('../../assets/logosenai.png')} 
-          resizeMode="contain"
-          style={{width: '100%', height: undefined, aspectRatio: 4}} 
+    <View style={styles.content}>
+      <View style={styles.loginCard}>
+        <View style={styles.logoContainer}>
+          <Image
+            source={require("../../assets/logosenai.png")}
+            resizeMode="contain"
+            style={{ width: "100%", height: undefined, aspectRatio: 4 }}
           />
         </View>
-          </View>
 
+        <View style={styles.passwordContainer}>
           <TextInput
-            style={styles.input}
+            style={styles.passwordInput}
             placeholder="Email"
             value={user.email}
             onChangeText={(value) => {
               setUser({ ...user, email: value });
             }}
-          />
-          
-          <TextInput
-            style={styles.input}
-            placeholder="Senha"
-            secureTextEntry={true}
-            value={user.password}
-            onChangeText={(value) => {
-              setUser({ ...user, password: value });
-            }}
-          />
-          
-          <TouchableOpacity onPress={handleLogin} style={styles.loginButton}>
-            <Text style={styles.loginButtonText}>Entrar</Text>
-          </TouchableOpacity>
-          
-          <View style={styles.signupContainer}>
-            <Text style={styles.signupText}>Não tem cadastro? </Text>
-            <TouchableOpacity onPress={() => navigation.navigate("Cadastro")}>
-              <Text style={styles.signupLinkText}>Cadastrar-se</Text>
-            </TouchableOpacity>
-          </View>
+          ></TextInput>
         </View>
-      </View>
-      
-      {/* Footer */}
-      <View style={styles.footer}>
+
+        <View style={styles.passwordContainer}>
+          <TextInput
+            style={styles.passwordInput}
+            placeholder="Senha"
+            value={user.senha}
+            secureTextEntry={user.showPassword}
+            onChangeText={(value) => {
+              setUser({ ...user, senha: value });
+            }}
+          ></TextInput>
+          <TouchableOpacity
+            onPress={() =>
+              setUser({ ...user, showPassword: !user.showPassword })
+            }
+          >
+            <Ionicons
+              name={user.showPassword ? "eye-off-outline" : "eye-outline"}
+              size={24}
+              color="gray"
+            />
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity onPress={handleLogin} style={styles.loginButton}>
+          <Text style={styles.loginButtonText}> Entrar </Text>
+        </TouchableOpacity>
+
+        <View style={styles.signupContainer}>
+          <Text style={styles.signupText}>Não tem cadastro? </Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Cadastro")}>
+            <Text style={styles.signupLinkText}>Cadastrar-se</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFF5F5",
-  },
-  logo:{
+  logo: {
     width: 270,
     height: 150,
-  },
-  header: {
-    backgroundColor: "#CC1E1E",
-    width: "100%",
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  headerTitle: {
-    color: "white",
-    fontSize: 18,
-    fontWeight: "bold",
   },
   content: {
     flex: 1,
@@ -131,6 +134,22 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     paddingHorizontal: 15,
   },
+  passwordContainer: {
+    width: "100%",
+    height: 45,
+    backgroundColor: "white",
+    borderRadius: 25,
+    marginVertical: 8,
+    paddingHorizontal: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    paddingRight: 10,
+  },
+  passwordInput: {
+    flex: 1,
+    height: 40,
+  },
   loginButton: {
     width: "100%",
     height: 45,
@@ -159,9 +178,4 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: "bold",
   },
-  footer: {
-    backgroundColor: "#CC1E1E",
-    width: "100%",
-    height: 50,
-  }
 });
